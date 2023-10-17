cb49d8e2aaa2ba0b54232123d307e304388d0901
Maroswen <marcus.albanese3@gmail.com>
2023-10-17 02:29:50 -0400

    styled up the site

--- js/site.js
+++ js/site.js
@@ -1,13 +1,46 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
-
-    Swal.fire(
-        {
+// get the user's message to reverse
+function getValues() {
+    // get the text input from the page
+    let message = document.getElementById('userInput').value;
+    
+    // validate the input: make sure its not empty
+    if (message.length == 0) {
+        Swal.fire({
+            icon: 'error',
             backdrop: false,
-            title: 'APP NAME',
-            text: msg, // <-- replace this text with that variable
-        }
-    );
+            title: 'Oops!',
+            text: 'Please enter a message to reverse'
+        });
+    } else {
+        // pass the input to reverseMessage and assign its return value to a variable
+        let revMsg = reverseMessage(message);
+        
+        // give the reversed message to displayMessage
+        displayMessage(revMsg);
+
+    }
+
+}
+
+// reverse the string 
+function reverseMessage(input) { // input = 'hello'
+    
+    let output = '';
+
+    for (let i = input.length - 1; i >= 0; i--) {
+        
+        output += input[i];
+
+    }
+
+    return output;
+}
+
+// display the reversed message
+function displayMessage(message) {
+
+    document.getElementById('msg').textContent = `Your message reflected is: ${message}`;
+
+    document.getElementById('alert').classList.remove('invisible');
 
 }
